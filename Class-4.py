cbf937a5c6d3a862a43abb51735e8afe32c886d7
Mantri Vamsikrishna <vamsikrishnamantri@gmail.com>
2025-01-08 17:35:24 +0530

    Update Class-4.py
    python code

--- Class-4.py
+++ Class-4.py
@@ -1,1 +1,39 @@
+# Variable: It is a space where we can able to store any value or data.
+# Identifier: Identifier is a identity to the variable/class/function etc.
+a=10;# here a is an identity to a variable where we are storing 10 in it.
+a
 
+#print() is a predefined function that is using to display any data as an output.
+b=20
+print(a)
+print(b)
+
+#id() it is also a predefined function that used to get an unique id number to the variabes.
+print(id(a))
+print(id(b))
+
+#In python Capital A and Small a are different because it is a case sensitive language.
+A=10
+a=30
+print(A)
+print(a)
+
+#Rule of the identifiers or naming convensions
+ #1-We can define the identifier name with both uppercase and lowercase letters. 
+A=50
+b=60
+ #2-We can start the identifier name with uppercase and lowercase letters and also with the special character _ 
+_h=80
+ #3-Only _ will be used in the identifier names, no other characters are not allowed.
+ #4-keywords are not used as identifiers.keywords are the predefined words that have it's own defination or meaning or specific task
+ #5-In python we are having 35 keywords. All the keywords are started with small letters only except True, False, None.
+ #6-spaces are not allowed in between the identifier names.
+ #7-We can't able to start the identifier names with numbers, but we can used in the middle of the identifiers.
+ #8-We can give any length to a variabe name.
+print(A)
+print(b)
+print(_h)
+
+#we are having two types of functions such as built-in functions and user defined functions
+#We are having two tyes of modules such as built-in modules and user defined modules.
+#We are having two types of classes such as built-in classes and user defined classes
